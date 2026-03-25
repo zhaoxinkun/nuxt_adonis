d1fe82e9897fcc392ca3daee134968949d36d0c6
zhaoxinkun <zhaoxinkun1124@gmail.com>
2026-03-25 19:29:57 +0800

    orderByRaw

--- database/factories/article_factory.ts
+++ database/factories/article_factory.ts
@@ -1,12 +1,14 @@
 import factory from '@adonisjs/lucid/factories'
 import Article from '#models/article'
+import Category from '#models/category'
 
 export const ArticleFactory = factory
   .define(Article, async ({ faker }) => {
+    const category = await Category.query().orderByRaw('rand()').first()
     return {
       title: faker.lorem.sentence(),
       content: faker.lorem.paragraph(),
-      category_id: 1,
+      category_id: category?.id || 1,
     }
   })
   .build()
